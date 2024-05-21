129ee0e49689c77aa574b1a59faea2ecb9c3a7e8
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2024-05-21 19:40:27 -0400

    adds new Playlist and Playlists component displaying the contents of one playlist or multiple adds routes to these new components adds a services folder with two service get playlist which links to our show action and get playlists which links to our index action adds Playlist type

---
+++ app/javascript/components/Playlist.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPlaylist } from '../services';
+import { Playlist } from '../types/playlist';
+import { Link } from 'react-router-dom';
+
+export default (): JSX.Element => {
+
+    const { id } = useParams();
+    const [playlist, setPlaylist] = useState<Playlist | null>();
+
+    useEffect(() => {
+        const fetchPlaylist = async (): Promise<void> => {
+            const playlist = await getPlaylist(Number(id));
+            setPlaylist(playlist);
+        };
+
+        fetchPlaylist();
+    }, []);
+
+    if (!playlist) {
+        return <></>;
+    }
+
+    return(
+        <div>
+            <h2 className='text-center'>Playlist: {playlist.name}</h2>
+            <div className='d-flex justify-content-center'>
+                <Link to='/' className='btn btn-lg custom-button mt-2'>Navigate Home</Link>
+            </div>
+        </div>
+    );
+};
